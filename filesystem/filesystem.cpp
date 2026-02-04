4c0e965c95712221c9ea4623bdc07782c55abf30
cangcang2025 <211928879+cangcang-zh-2025@users.noreply.github.com>
2026-02-04 20:31:11 +0800

    Create filesystem.cpp

---
+++ filesystem/filesystem.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file filesystem.cpp
+ * @brief 文件系统管理器实现
+ * @author 云流操作系统开发团队
+ * @date 2026-02-04
+ * @version 1.0.0
+ * 
+ * 文件系统管理器的具体实现，包括挂载管理、文件操作、目录遍历等功能
+ */
+
+#include "filesystem.h"
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <mutex>
+#include <thread>
+#include <system_error>
+#include <fcntl.h>
+#include <unistd.h>
+
+namespace CloudFlow::File
